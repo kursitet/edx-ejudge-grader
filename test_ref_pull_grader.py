2823ffe1ccd1a4674c56f5aad06128f4d81c0fe7
madker4 <madker4@gmail.com>
2017-04-04 19:22:11 +0300

    problem param

--- test_ref_pull_grader.py
+++ test_ref_pull_grader.py
@@ -230,9 +230,9 @@ def create_dir_structure(contest_id):
     return path_contest
 
 
-def create_serve_cfg(contest_name, contest_id, path_contest,lang_name):
+def create_serve_cfg(path_contest, lang_name, problem_name):
     serve = open(path_contest + 'conf/serve.cfg', 'w')
-    global_param = list('# -*- coding: utf-8 -*-', '# $Id$', 'contest_time = 0',
+    global_param = ['# -*- coding: utf-8 -*-', '# $Id$', 'contest_time = 0',
                  'score_system = acm',
                  'compile_dir = "../../compile/var/compile"',
                  'team_enable_rep_view',
@@ -244,19 +244,22 @@ def create_serve_cfg(contest_name, contest_id, path_contest,lang_name):
                  'advanced_layout',
                  'enable_l10n',
                  'team_download_time = 0',
-                 'cpu_bogomips = 4533',)
+                 'cpu_bogomips = 4533']
     lang_param = get_lang_param(lang_name)
-
-
+    problem_param = get_problem_param(problem_name)
+    tester_param = get_tester_param()
+    all_param = global_param.extend(lang_param).extend(problem_param).extend(tester_param)
+    for row in all_param:
+        serve.write(row)
 
 
 def get_lang_param(lang_short_name):
     lang_id = get_lang_id(lang_short_name)
-    param = list()
-    param.append('[language]')
+    param = ['[language]']
     file = open('./programm_lang/' + lang_id, 'r')
     param.extend(list(file))
     return param
+
 
 def get_lang_id(lang_short_name):
     file = open('./lang_short_to_id.csv', 'r')
@@ -265,11 +268,60 @@ def get_lang_id(lang_short_name):
         if lang['name'] == lang_short_name:
             return lang['id']
 
-def get_problem_param(problem_name):
-    pass
 
-def get_problem_id():
-    pass
+def get_problem_param(problem_name):
+    param = [
+        '[problem]',
+        'short_name = ' + '"'+problem_name+'"',
+        'long_name = ""',
+        'scoring_checker = 0',
+        'interactive_valuer = 0',
+        'manual_checking = 0',
+        'check_presentation = 0',
+        'use_stdin',
+        'combined_stdin = 0',
+        'use_stdout',
+        'combined_stdout = 0',
+        'binary_input = 0',
+        'ignore_exit_code = 0',
+        'test_sfx = ".dat"',
+        'test_pat = ""',
+        'use_corr',
+        'corr_sfx = ".ans"',
+        'corr_pat = ""',
+        'use_info = 0',
+        'info_sfx = ""',
+        'info_pat = ""',
+        'use_tgz = 0',
+        'tgz_sfx = ""',
+        'tgz_pat = ""',
+        'tgzdir_sfx = ""',
+        'tgzdir_pat = ""',
+        'standard_checker = "cmp_int_seq"',
+        'disable_auto_testing = 0',
+        'disable_testing = 0',
+        'enable_compilation = 0',
+        'valuer_sets_marked = 0',
+        'disable_stderr = 0',
+        'normalization = "nl"'
+    ]
+    return param
+
+
+def get_tester_param():
+    param = ['[tester]',
+             'any',
+             'no_core_dump',
+             'kill_signal = KILL',
+             'memory_limit_type = "default"',
+             'secure_exec_type = "static"',
+             'clear_env',
+             'start_env = "PATH=/usr/local/bin:/usr/bin:/bin"',
+             'start_env = "HOME"',
+             'check_dir = "TWD"',
+             ]
+    return param
+
 
 def del_str_in_xml(name_file):
     file = open('./report/' + name_file, 'r')
